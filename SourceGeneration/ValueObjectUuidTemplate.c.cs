8e9661059cffb3759c4a733cc69703bf19904a0e
ma310kano <ma310kano@gmail.com>
2022-11-16 22:38:59 +0900

    プロジェクト ファイルを追加します。

---
+++ SourceGeneration/ValueObjectUuidTemplate.c.cs
@@ -0,0 +1,30 @@
+﻿namespace SourceGeneration
+{
+    /// <summary>
+    /// 値オブジェクト(UUID)のテンプレート
+    /// </summary>
+    public partial class ValueObjectUuidTemplate
+    {
+        #region Fields
+
+        /// <summary>
+        /// コンテキスト
+        /// </summary>
+        private readonly ValueObjectUuidContext _context;
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// 値オブジェクト(UUID)のテンプレートを初期化します。
+        /// </summary>
+        /// <param name="context">コンテキスト</param>
+        public ValueObjectUuidTemplate(ValueObjectUuidContext context)
+        {
+            _context = context;
+        }
+
+        #endregion
+    }
+}
