b9d99050876dfa9457e24d46a1ba80f8b6875a0f
ma310kano <ma310kano@gmail.com>
2022-12-30 19:00:12 +0900

    不変オブジェクトへ変更

--- SourceGeneration/Port.Adapters/DataContextProperty.cs
+++ SourceGeneration/Port.Adapters/DataContextProperty.cs
@@ -5,22 +5,42 @@
     /// </summary>
     public class DataContextProperty
     {
+        #region Constructors
+
+        /// <summary>
+        /// データのコンテキストのプロパティを初期化します。
+        /// </summary>
+        /// <param name="typeName">型名</param>
+        /// <param name="propertyNameEnglish">プロパティ名(英)</param>
+        /// <param name="propertyNameJapanese">プロパティ名(日)</param>
+        public DataContextProperty(
+            string typeName,
+            string propertyNameEnglish,
+            string propertyNameJapanese)
+        {
+            TypeName = typeName;
+            PropertyNameEnglish = propertyNameEnglish;
+            PropertyNameJapanese = propertyNameJapanese;
+        }
+
+        #endregion
+
         #region Properties
 
         /// <summary>
         /// 型名を取得します。
         /// </summary>
-        public string TypeName { get; set; }
+        public string TypeName { get; }
 
         /// <summary>
         /// プロパティ名(英)を取得します。
         /// </summary>
-        public string PropertyNameEnglish { get; set;  }
+        public string PropertyNameEnglish { get; }
 
         /// <summary>
         /// プロパティ名(日)を取得します。
         /// </summary>
-        public string PropertyNameJapanese { get; set; }
+        public string PropertyNameJapanese { get; }
 
         /// <summary>
         /// コンストラクターの引数名を取得します。
